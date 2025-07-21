5c296bcce48152e1886037612f3e262ff04d4aa6
Marko VOJINOVIC <marko.vojinovic@maxbet.rs>
2025-07-21 18:18:34 +0200

    Change some types

--- test-data/users/registration_user.ts
+++ test-data/users/registration_user.ts
@@ -1,14 +1,31 @@
-export default {
-    firstName: "First_Name",
-    lastName: "Last_name",
-    username: "username52",
-    password: "1112",
-    confirmPass: "1112",
-    passwordWrong: "111",
-    passwordWrong1: "3333",
-    confirmPassWrong: "2222",
-    email: 'testemail@test.com',
-    phoneNumber: '1111111',
-    username33: 'Dina20',
-    password33: 's3cret'
-  }
+export interface RegistrationUserData {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  confirmPass: string;
+  passwordWrong: string;
+  passwordWrong1: string;
+  confirmPassWrong: string;
+  email: string;
+  phoneNumber: string;
+  username33: string;
+  password33: string;
+}
+
+const registrationUserData: RegistrationUserData = {
+  firstName: "First_Name",
+  lastName: "Last_name",
+  username: "username52",
+  password: "1112",
+  confirmPass: "1112",
+  passwordWrong: "111",
+  passwordWrong1: "3333",
+  confirmPassWrong: "2222",
+  email: "testemail@test.com",
+  phoneNumber: "1111111",
+  username33: "Dina20",
+  password33: "s3cret"
+};
+
+export default registrationUserData;
